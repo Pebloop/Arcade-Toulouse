8df0bf3084fc1879dcd20831b68ef9b45c37a0fb
Ydos2 <Ydos2@users.noreply.github.com>
2021-04-06 20:48:55 +0000

    deploy: fe43450393684b817b52b82225a074cd645a0f7d

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['foreach_62',['forEach',['../classarcade_1_1_i_entity.html#ad26132eb253a762eb839dcc02f19c4f2',1,'arcade::IEntity::forEach()'],['../classarcade_1_1_i_scene.html#a8702bffd4b59a6a3785750c9ae6fb4fa',1,'arcade::IScene::forEach()']]]
+  ['foreach_62',['forEach',['../classarcade_1_1_i_entity.html#ad26132eb253a762eb839dcc02f19c4f2',1,'arcade::IEntity::forEach()'],['../classarcade_1_1_i_scene.html#a8702bffd4b59a6a3785750c9ae6fb4fa',1,'arcade::IScene::forEach(std::function&lt; void(IEntity &amp;)&gt;)=0'],['../classarcade_1_1_i_scene.html#a904c4fb1f13d13c044202721746f3581',1,'arcade::IScene::forEach(std::function&lt; void(IEntity &amp;)&gt;, const std::string &amp;name)=0']]]
 ];
